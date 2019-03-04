f262bb62f901d7946d2a11e0c67ac70282678e68
mrcliffster <tristan.cliff@hotmail.com>
2019-03-04 14:43:22 -0800

    Initial Commit
    Linked to remote

---
+++ Assets/Scripts/Universe.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarTraders
+{
+    public class Universe
+    {
+        private List<SolarSystem> systems;
+
+        public List<SolarSystem> Systems
+        {
+            get
+            {
+                return systems;
+            }
+        }
+
+        private int numberSystems;
+
+        public Universe(int systemCount = 1)
+        {
+            systems = new List<SolarSystem>(systemCount);
+        }
+
+        public void AddSolarSystem()
+        {
+            systems.Add(new SolarSystem("medium"));
+        }
+    }
+
+    public class SolarSystem
+    {
+        private List<PlanetaryBody> bodies;
+        string size; // Change to enum? One of: Small/Med/Large/Huge
+        int maximum_radius;
+        int numberBodies; // max number bodies = size max + 1 for star
+        public SolarSystem(string size)
+        {
+            this.size = size;
+            bodies = new List<PlanetaryBody>
+            {
+                new Star(Star.GetStarType())
+            };
+            GeneratePlanets();
+        }
+
+        private void GeneratePlanets()
+        {
+            Random random = new Random();
+            int numPlanets = 3;
+            switch (size)
+            {
+                case "small":
+                    numPlanets = random.Next(3, 5);
+                    break;
+                case "medium":
+                    numPlanets = random.Next(4, 6);
+                    break;
+                case "large":
+                    numPlanets = random.Next(5, 7);
+                    break;
+                case "huge":
+                    numPlanets = random.Next(7, 11);
+                    break;
+            }
+
+            for (int i = 0; i < numPlanets; i++)
+            {
+                if (i == 2) // third rock from the sun! Randomise this eventually
+                {
+                    bodies.Add(new Planet(true, Planet.GetPlanetType()));
+                }
+                else
+                {
+                    bodies.Add(new Planet(false, Planet.GetPlanetType()));
+                }
+            }
+        }
+    }
+
+    public abstract class PlanetaryBody
+    {
+        string name;
+        string type;
+    }
+
+    public class Star : PlanetaryBody
+    {
+        string type; // Change to enum? Red dwarf, neutron, black hole, regular
+        string name;
+        public Star(string type)
+        {
+            this.type = type;
+        }
+
+        public static string GetStarType()
+        {
+            // Insert random star type logic here?
+            return "regular";
+        }
+    }
+
+    public class Planet : PlanetaryBody
+    {
+        string name;
+        string type; // Change to enum?
+        List<ResourceDeposit> deposits;
+        List<Moon> moons;
+        bool colonised;
+        public bool hasBelt;
+
+        // Constructor
+        public Planet(bool isColonised, string type)
+        {
+            colonised = isColonised;
+            this.type = type;
+            MakeMoons();
+
+        }
+
+        public void addDeposit(string type, string size)
+        {
+            deposits.Add(new ResourceDeposit(type, size));
+        }
+
+        private void MakeMoons()
+        {
+            Random random = new Random();
+            for (int i = 0; i < random.Next(0, 2); i++)
+            {
+                moons.Add(new Moon());
+            }
+        }
+
+        public static string GetPlanetType()
+        {
+            return "solid"; // change to include gas, asteroid
+        }
+    }
+
+    public class Moon : PlanetaryBody
+    {
+    }
+}
+
+public class ResourceDeposit
+{
+    string type; // Change to enum? One of Metals, Gasses, Food
+    string size; // Change to enum? One of: Small/Med/Large/Huge
+    float startingSize;
+    public ResourceDeposit(string type, string size)
+    {
+        this.type = type;
+        this.size = size;
+        switch (size) // Add in configuration file system to change the hard-coded values
+        {
+            case "small":
+                startingSize = 200;
+                break;
+            case "medium":
+                startingSize = 500;
+                break;
+            case "large":
+                startingSize = 1000;
+                break;
+            case "huge":
+                startingSize = 2000;
+                break;
+        }
+
+    }
+}
+
+
---
+++ Assets/Scripts/UniverseManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SolarTraders;
+
+public class UniverseManager : MonoBehaviour
+{
+    Universe uni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uni = new Universe();
+        uni.AddSolarSystem();
+        Init();
+    }
+
+    void Init()
+    {
+        for (int i = 0; i < uni.Systems.Count; i++)
+        {
+            uni.Systems.get
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
